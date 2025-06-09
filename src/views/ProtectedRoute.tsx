cda28474b032693b1703dad6d6a349c1ce98d317
semilore <semilore@omotade-michaels.com>
2025-06-09 13:23:40 +0100

    fix: build error

--- src/views/ProtectedRoute.tsx
+++ src/views/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../utils/AuthContext";
 import type { ReactNode } from "react";
 
